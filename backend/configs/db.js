be8953d5047f336fa4598de15a546f7bea58c13b
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-04-17 17:54:47 +0530

    apartment-manager

---
+++ backend/configs/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    return mongoose.connect("mongodb+srv://AJIT:AJIT354@cluster0.5sdkb.mongodb.net/APARTMEN");
+}
+
+
+//mongodb+srv://ajit:ajit1122@cluster0.lb1g0.mongodb.net/MANAGER
